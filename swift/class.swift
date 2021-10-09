e6767df5372653b5ebee6903cd5c28c463b69892
Nico Prasasty S <>
2021-10-09 14:59:13 +0700

    Feature

---
+++ swift/class.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+class Hactoberfest2021{
+    
+}
+
+let hactoberfest2021 = Hactoberfest2021()
---
+++ swift/classStatic.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class StaticHactoberfest2021{
+    public static func run()->String{
+        "is my turn"
+    }
+}
+
+let staticHacktoberfest2021 = staticHacktoberfest2021()
+
+print(staticHacktoberfest2021.run())
